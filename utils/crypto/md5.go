7f33048fca23893882b3b5f264164704a9b81f13
DoNetKit <donetkit@gmail.com>
2023-07-14 11:04:48 +0800

    crypto

--- utils/crypto/md5.go
+++ utils/crypto/md5.go
@@ -2,6 +2,7 @@ package crypto
 
 import (
 	"crypto/md5"
+	"encoding/hex"
 	"fmt"
 )
 
@@ -16,4 +17,18 @@ func Md5String(str string) string {
 	data := []byte(str)
 	has := md5.Sum(data)
 	return fmt.Sprintf("%x", has) //将[]byte转成16进制
+}
+
+// MD5Encode16 返回一个16位md5加密后的字符串
+func MD5Encode16(data string) string {
+	h := md5.New()
+	h.Write([]byte(data))
+	bys := h.Sum(nil)
+	res := make([]byte, 0)
+	for i := 0; i < len(bys); i++ {
+		if i%2 != 0 {
+			res = append(res, bys[i])
+		}
+	}
+	return hex.EncodeToString(res)
 }
