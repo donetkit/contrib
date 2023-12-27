e71e65de6437336b9105acdf6cd3f514970c93a0
DoNetKit <donetkit@gmail.com>
2023-12-27 10:47:20 +0800

    verify ip
    verify ip

--- utils/host/ip.go
+++ utils/host/ip.go
@@ -2,6 +2,7 @@ package host
 
 import (
 	"net"
+	"regexp"
 	"strings"
 )
 
@@ -57,4 +58,24 @@ func GetLocalIPs() []string {
 		}
 	}
 	return ip
+}
+
+func VerifyIpV4(ip string) bool {
+	comp := regexp.MustCompile(`([1-9]?\d|1\d{2}|2[0-4]\d|25[0-5])(\.([1-9]?\d|1\d{2}|2[0-4]\d|25[0-5])){3}$`)
+	subMatch := comp.FindAllStringSubmatch(ip, -1)
+	if len(subMatch) <= 0 {
+		return false
+	}
+	return true
+}
+
+func VerifyIpV6(ip string) bool {
+	ip = strings.ReplaceAll(ip, "[", "")
+	ip = strings.ReplaceAll(ip, "]", "")
+	comp := regexp.MustCompile(`^(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:([0-9a-fA-F]{1,4}|:)|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]).){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))$`)
+	subMatch := comp.FindAllStringSubmatch(ip, -1)
+	if len(subMatch) <= 0 {
+		return false
+	}
+	return true
 }
